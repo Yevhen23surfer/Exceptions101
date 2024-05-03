3ee2086d6b7d25ebb73efc5e5eaf138a99339ff7
Yevhen Surfer <23skopets@gmail.com>
2024-05-03 16:55:17 +0200

    Catch multiple exceptions in a code block

--- Program.cs
+++ Program.cs
@@ -18,11 +18,11 @@ static void Process1()
 {
     try
     {
-        WriteMessage(); 
+        WriteMessage();
     }
-    catch
+    catch (DivideByZeroException ex)
     {
-        Console.WriteLine("Exception caught in Process1");
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
     }
 }
 
@@ -32,7 +32,12 @@ static void WriteMessage()
     double float2 = 0.0;
     int number1 = 3000;
     int number2 = 0;
+    byte smallNumber;
 
     Console.WriteLine(float1 / float2);
     Console.WriteLine(number1 / number2);
+    checked
+    {
+        smallNumber = (byte)number1;
+    }
 }
